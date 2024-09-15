3f4bde99dd200e53a14586222993c37e36abeaa3
YvanBlanchette <yvanblanchette@gmail.com>
2024-09-15 12:01:56 -0400

    initial commit

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,19 +1,15 @@
-'use client';
+"use client";
 
-import { Button } from '@/components/ui/button';
-import Link from 'next/link';
-import { usePathname } from 'next/navigation';
-import { FaShopware } from 'react-icons/fa6';
-import { BiSolidShoppingBagAlt } from 'react-icons/bi';
-import { useShoppingCart } from 'use-shopping-cart';
-import { DropdownCategories } from './Dropdown';
-import { BsList } from 'react-icons/bs';
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { FaShopware } from "react-icons/fa6";
+import { BiSolidShoppingBagAlt } from "react-icons/bi";
+import { useShoppingCart } from "use-shopping-cart";
+import { DropdownCategories } from "./Dropdown";
+import { BsList } from "react-icons/bs";
 
-const links = [
-	{ name: 'Accueil', href: '/' },
-	{ name: 'À Propos', href: '/About' },
-	{ name: 'Nous Joindre', href: '/Contact' },
-];
+const links = [{ name: "Accueil", href: "/" }];
 
 const Navbar = () => {
 	const pathname = usePathname();
@@ -21,40 +17,42 @@ const Navbar = () => {
 	const { handleCartClick } = useShoppingCart();
 
 	return (
-		<header className='mb-8 border-b shadow-lg'>
-			<div className='flex items-center justify-between mx-auto max-w-2xl px-4 sm:px-6 lg:max-w-7xl'>
-				<Link href='/' className='flex items-center text-2xl sm:text-4xl gap-2'>
-					<FaShopware className='text-primary' />
-					<h1 className='font-bold'>
-						Next<span className='font-medium text-primary'>Style</span>
+		<header className="mb-8 border-b shadow-lg">
+			<div className="flex items-center justify-between mx-auto max-w-2xl px-4 sm:px-6 lg:max-w-7xl">
+				<Link href="/" className="flex items-center text-2xl sm:text-4xl gap-2">
+					<FaShopware className="text-primary" />
+					<h1 className="font-bold">
+						Next<span className="font-medium text-primary">Style</span>
 					</h1>
 				</Link>
 
-				<nav className='hidden gap-12 lg:flex 2xl:ml-16'>
-					{links.map((link, index) => (
-						<div
-							key={index}
-							className={`font-medium text-xl hover:text-primary transition duration-200 ${
-								pathname === link.href ? 'text-primary pointer-events-none' : 'text-gray-900'
-							}`}
+				<div className="flex items-center gap-10">
+					<nav className="hidden gap-12 lg:flex 2xl:ml-16">
+						{links.map((link, index) => (
+							<div
+								key={index}
+								className={`font-medium text-xl hover:text-primary transition duration-200 ${
+									pathname === link.href ? "text-primary pointer-events-none" : "text-gray-900"
+								}`}
+							>
+								<Link href={link.href}>{link.name}</Link>
+							</div>
+						))}
+						<DropdownCategories />
+					</nav>
+					<div className="flex justify-end items-center gap-4">
+						<Button
+							onClick={() => handleCartClick()}
+							title="Voir mon panier"
+							variant={"ghost"}
+							className="flex flex-col gap-y-1.5 h-12 w-12 sm:h-20 sm:w-20 md:h-24 md:w-24 rounded-none"
 						>
-							<Link href={link.href}>{link.name}</Link>
-						</div>
-					))}
-					<DropdownCategories />
-				</nav>
-				<div className='flex justify-end items-center gap-4'>
-					<Button
-						onClick={() => handleCartClick()}
-						variant={'ghost'}
-						className='flex flex-col gap-y-1.5 h-12 w-12 sm:h-20 sm:w-20 md:h-24 md:w-24 rounded-none'
-					>
-						<BiSolidShoppingBagAlt className='text-primary text-3xl' />
-						<p className='text-gray-900 font-semibold hidden sm:block'>Voir Panier</p>
-					</Button>
-					<button className='inline-block lg:hidden h-12 w-12 sm:h-20 sm:w-20 md:h-24 md:w-24 bg-transparent'>
-						<BsList className='text-3xl' />
-					</button>
+							<BiSolidShoppingBagAlt className="text-primary text-3xl" />
+						</Button>
+						<button className="inline-block lg:hidden h-12 w-12 sm:h-20 sm:w-20 md:h-24 md:w-24 bg-transparent">
+							<BsList className="text-3xl" />
+						</button>
+					</div>
 				</div>
 			</div>
 		</header>
